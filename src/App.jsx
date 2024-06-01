4f7c67cd94858ae309cc81e11c61ec172f4e3b21
Dimitri Chupryna <dmytro.chupryna.1@gmail.com>
2024-06-01 22:43:24 +0100

    complete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
+import Box from './components/Box'
+import boxes from './boxes.js'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+  const [boxesState, setBoxesState] = useState(boxes) // set the boxes state to the boxes array
+  
+  function toggle(id) {
+    setBoxesState(prevBoxesState => {
+      return prevBoxesState.map(box => {
+        return box.id === id ? {...box, on: !box.on} : box
+      })
+    })
+  }
+  
+  const boxesElements = boxesState.map(box => { // map over the boxes array
+    return <Box 
+              key={box.id}
+              on={box.on}
+              toggle={() => toggle(box.id)}
+    />
+  })
   return (
     <>
-      
+      {boxesElements}
     </>
   )
 }
---
+++ src/components/Box.jsx
@@ -0,0 +1,12 @@
+export default function Box(props) {
+
+const styles = {
+    backgroundColor: props.on ? 'white' : 'black'
+}
+
+    return (
+        <div className="box" style={styles} onClick={props.toggle}>
+            
+        </div>
+    )
+}
